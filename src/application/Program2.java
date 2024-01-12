169adf9f3359f9157b941ee3d8edaff233445838
Matheus <matheusdabmlj@gmail.com>
2024-01-12 18:46:05 -0300

    Department findById Implementation

---
+++ src/application/Program2.java
@@ -0,0 +1,17 @@
+package application;
+
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
+import model.entities.Department;
+
+
+public class Program2 {
+    public static void main(String[] args) {
+
+        DepartmentDao DepartmentDao = DaoFactory.createDepartmentDao();
+
+        System.out.println("=== TEST 1: seller findById ===");
+        Department department = DepartmentDao.findById(3);
+        System.out.println(department);
+    }
+}
---
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -0,0 +1,78 @@
+package model.dao.impl;
+
+import db.DB;
+import db.DbException;
+import model.dao.DepartmentDao;
+import model.entities.Department;
+import model.entities.Seller;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+public class DepartmentDaoJDBC implements DepartmentDao {
+
+    private Connection conn;
+
+    public DepartmentDaoJDBC(Connection conn) {
+        this.conn = conn;
+    }
+
+
+    @Override
+    public void insert(Department obj) {
+
+    }
+
+    @Override
+    public void update(Department obj) {
+
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+
+    }
+
+    @Override
+    public Department findById(Integer id) {
+        PreparedStatement st = null;
+        ResultSet rs = null;
+
+        try {
+            st = conn.prepareStatement(
+                    "SELECT * "
+                            + "FROM department "
+                            + "WHERE Id = ?"
+            );
+            st.setInt(1,id);
+            rs = st.executeQuery();
+
+            if (rs.next()) { //testa se veio algum resultado do banco de dados. ou seja se tem um seller com esse id
+                Department dep = instantiateDepartment(rs);
+                return dep;
+            }
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        }
+        finally {
+            DB.closeStatment(st);
+            DB.closeResultSet(rs);
+        }
+        return null;
+    }
+
+    private Department instantiateDepartment(ResultSet rs) throws SQLException {
+        Department dep = new Department();
+        dep.setId(rs.getInt("Id"));
+        dep.setName(rs.getString("Name"));
+        return dep;
+    }
+
+    @Override
+    public List<Department> findAll() {
+        return null;
+    }
+}
